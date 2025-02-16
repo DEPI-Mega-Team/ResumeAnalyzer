5ac7e04daa080ff2b78601a6a041abe04c5f7752
ahmedelhefnawy <ahmedelhefnawy2003@yahoo.com>
2025-02-16 09:09:37 +0200

    Modified main.py
    Specified the input parameter class as UploadFile in the /parse endpoint.
    Added description to the /parse endpoint.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body, File
-
+from fastapi import UploadFile  
+from fastapi.responses import HTMLResponse
 import io
 import uvicorn
 from resume_analyzer import init_parser
@@ -10,8 +12,9 @@ app = FastAPI()
 parser = init_parser()
 
 
-@app.post("/parse")
-async def parse(resume = File(...)):
+@app.post("/parse", description="Parse resume and return the extracted information")
+async def parse(resume: UploadFile = File(...)):
+    
     resume_content = await resume.read()
     resume_io = io.BytesIO(resume_content)
     resume_io.name = resume.filename
@@ -19,6 +22,18 @@ async def parse(resume = File(...)):
     result = parser.parse(resume_io)
     return {'result': result}
 
+@app.get("/", description="Home route", response_class=HTMLResponse)
+async def home():
+    return HTMLResponse(content="""
+    <html>
+        <head>
+            <title>Hello world</title>
+        </head>
+        <body>
+            <h1>Hello world</h1>
+        </body>
+    </html>
+    """, status_code=200)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
