2c63ea3cb651c815779e88bc50304fbcbde77f75
ahmedelhefnawy <ahmedelhefnawy2003@yahoo.com>
2025-05-10 10:31:02 +0300

    Added Streamlit Interface

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from streamlit_tags import st_tags
+import io
+
+if 'parser' not in st.session_state:
+    from resume_analyzer import ResumeParser
+    st.session_state.parser = ResumeParser()
+
+if 'data' not in st.session_state:
+    st.session_state.data = None
+
+parser = st.session_state.parser
+resume = st.file_uploader("Upload your resume", type=["pdf", "docx"], accept_multiple_files= False)
+if not resume:
+    st.stop()
+
+parse_button = st.button("Parse Resume")
+if parse_button:
+    st.session_state.data = None
+    resume_content = resume.read()
+    resume_io = io.BytesIO(resume_content)
+    resume_io.name = resume.name
+    details = parser.parse(resume_io)
+    st.session_state.data = details
+
+if st.session_state.data:
+    for key, value in st.session_state.data.items():
+        if isinstance(value, list):
+            keywords = st_tags(label=key, value= value)
+            continue
+        if isinstance(value, float) or isinstance(value, int):
+            st.number_input(key, value= value)
+            continue
+        else:
+            st.text_input(key, value)
+
+
